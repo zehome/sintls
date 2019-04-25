4d7fd6f8a0503b723643186632e7edd0217967c6
Laurent Coustet <laurent.coustet@clarisys.fr>
2019-04-25 11:22:12 +0200

    code reorg: use cmd/* pattern

--- cmd/sintls-client/main.go
+++ cmd/sintls-client/main.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
 	"github.com/urfave/cli"
-	"github.com/zehome/sintls/cmd"
 )
 
 var (
@@ -29,9 +28,9 @@ func main() {
 		fmt.Printf("sintls version %s %s/%s\n", c.App.Version, runtime.GOOS, runtime.GOARCH)
 	}
 	defaultPath := filepath.Join(usr.HomeDir, ".config", "sintls")
-	app.Flags = cmd.CreateFlags(defaultPath)
-	app.Before = cmd.Before
-	app.Commands = cmd.CreateCommands()
+	app.Flags = CreateFlags(defaultPath)
+	app.Before = Before
+	app.Commands = CreateCommands()
 
 	selfupdate.EnableLog()
 
